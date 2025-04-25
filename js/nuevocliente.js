f2ac7c02b70c186929f7035921d3910dcc1f5974
JeanCarlos <jmatochepascual@gmail.com>
2025-04-25 15:34:50 -0500

    feat: :recycle: Update logic

--- js/nuevocliente.js
+++ js/nuevocliente.js
@@ -1,89 +1,75 @@
+const companyElement = document.querySelector('#empresa');
+const emailElement = document.querySelector('#email');
+const formElement = document.querySelector('#formulario');
+const nameElement = document.querySelector('#nombre');
+const phoneElement = document.querySelector('#telefono');
 let DB;
 
-// Selectores
-const formularioElement = document.querySelector('#formulario');
-const nombreElement = document.querySelector('#nombre');
-const emailElement = document.querySelector('#email');
-const telefonoElement = document.querySelector('#telefono');
-const empresaElement = document.querySelector('#empresa');
+const openConectionDB = () => {
+  const conectionDB = indexedDB.open('clientes', 1);
 
-
-// Abrir conexión con la base de datos
-const abrirConexionIndexDB = () => {
-  const abrirConexion = indexedDB.open('clientes', 1);
-
-  abrirConexion.onerror = () => {
-    // console.log('Error al abrir la base de datos');
+  conectionDB.onerror = () => {
+    console.log('Error al abrir la base de datos');
   };
 
-  abrirConexion.onsuccess = () => {
-    // console.log('Base de datos abierta');
-    DB = abrirConexion.result;
-  }
+  conectionDB.onsuccess = () => {
+    DB = conectionDB.result;
+  };
 };
 
-const init = (event) => {
+const startApp = (event) => {
   event.preventDefault();
 
-  const cliente = {
-    nombre: nombreElement.value.trim(),
+  const client = {
+    company: companyElement.value.trim(),
     email: emailElement.value.trim(),
-    telefono: telefonoElement.value.trim(),
-    empresa: empresaElement.value.trim(),
-    id: Date.now() + Math.random().toString(36).substring(2)
+    id: Date.now() + Math.random().toString(36).substring(2),
+    name: nameElement.value.trim(),
+    phone: phoneElement.value.trim(),
   };
 
-
-  if (validarCliente(cliente)) {
-    mostarAlerta('Error!', 'Todos los campos son obligatorios', false);
+  if (Object.values(client).includes('')) {
+    showAlert('Error!', 'Todos los campos son obligatorios', false);
     return;
-  }
+  };
 
-  // Agregar cliente a la base de datos
-  agregarClienteIndexDB(cliente);
+  createClient(client);
 };
 
-// Validar el formulario
-const validarCliente = (cliente) => Object.values(cliente).includes('') ? true : false;
+const showAlert = (typeMessage, message, type = true) => {
+  const hasAlert = document.querySelector('.alerta');
 
+  if (hasAlert) return;
 
-// Mostrar alerta en pantalla
-const mostarAlerta = (tipoMensaje, mensaje, tipo = true) => {
-  const alerta = document.querySelector('.alerta');
+  const alertElement = document.createElement('p');
 
-  if (!alerta) {
-    const alertaElement = document.createElement('p');
+  alertElement.innerHTML = ` 
+    <strong class="font-bold">${typeMessage}</strong>  <span class="block sm:inline">${message}</span>`;
+  alertElement.classList.add('alerta', 'text-center', 'mt-4', 'p-3', 'rounded', 'max-w-md', 'mx-auto');
 
-    alertaElement.innerHTML = ` 
-    <strong class="font-bold">${tipoMensaje}</strong>  <span class="block sm:inline">${mensaje}</span>`;
-    alertaElement.classList.add('alerta', 'text-center', 'mt-4', 'p-3', 'rounded', 'max-w-md', 'mx-auto');
+  type
+    ? alertElement.classList.add('text-green-700', 'bg-green-100', 'border-green-400')
+    : alertElement.classList.add('text-red-700', 'bg-red-100', 'border-red-400');
 
-    tipo
-      ? alertaElement.classList.add('text-green-700', 'bg-green-100', 'border-green-400')
-      : alertaElement.classList.add('text-red-700', 'bg-red-100', 'border-red-400');
+  formElement.appendChild(alertElement);
 
-    formularioElement.appendChild(alertaElement);
-
-    setTimeout(() => {
-      alertaElement.remove();
-    }, 3000);
-  }
+  setTimeout(() => {
+    alertElement.remove();
+  }, 3000);
 };
 
-// Agregar cliente a la base de datos
-const agregarClienteIndexDB = (cliente) => {
-
-  const transacción = DB.transaction(['clientes'], 'readwrite')
-  const objectStore = transacción.objectStore('clientes');
+const createClient = (cliente) => {
+  const transaction = DB.transaction(['clientes'], 'readwrite')
+  const objectStore = transaction.objectStore('clientes');
   objectStore.add(cliente);
 
-  transacción.onerror = () => {
-    mostarAlerta('Error!', 'Hubo un error al agregar el cliente', false);
+  transaction.onerror = () => {
+    showAlert('Error!', 'Hubo un error al agregar el cliente', false);
   };
 
-  transacción.oncomplete = () => {
-    formularioElement.reset();
-    mostarAlerta('Correcto!', 'El cliente se agregó correctamente', true);
+  transaction.oncomplete = () => {
+    formElement.reset();
+    showAlert('Correcto!', 'El cliente se agregó correctamente', true);
 
     setTimeout(() => {
       window.location.href = 'index.html';
@@ -92,6 +78,6 @@ const agregarClienteIndexDB = (cliente) => {
 };
 
 document.addEventListener('DOMContentLoaded', () => {
-  formularioElement.addEventListener('submit', init);
-  abrirConexionIndexDB();
+  openConectionDB();
+  formElement.addEventListener('submit', startApp);
 });
